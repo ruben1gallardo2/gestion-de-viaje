bb8618b5865fdc99196628f3de4a4a760ba02ef0
unknown <your-email>
2017-04-10 19:33:24 -0400

    intento de errores

--- app/Http/Controllers/VueloController.php
+++ app/Http/Controllers/VueloController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Vuelo as Vuelos;
+use App\Vuelo as Vuelo;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class VueloController extends Controller
 {
-  function index() {
-    $vuelos = Vuelos::all();
-    return view('index', ['vuelos' => $vuelos]);
-  }
-
-  function show($id = 1) {
-    $vuelo = Vuelos::findOrFail($id);
+  function index($id = 1) {
+    $vuelo = Vuelo::findOrFail($id);
     return view('vuelo', ['vuelo' => $vuelo]);
   }
+  
+  function show($id = 1) {
+    $vuelo = Vuelo::findOrFail($id);
+    return view('vuelo', ['vuelo' => $vuelo]);
+
+
+
+    }
 }
