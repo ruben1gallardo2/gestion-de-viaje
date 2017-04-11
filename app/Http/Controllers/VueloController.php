d3607ab3de113f01922e723976a7181f4d86771b
unknown <your-email>
2017-04-11 11:26:17 -0400

    epale alfredo

--- app/Http/Controllers/VueloController.php
+++ app/Http/Controllers/VueloController.php
@@ -9,16 +9,19 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class VueloController extends Controller
 {
-  function index($id = 1) {
+
+     function index() {
+       $vuelos = Vuelo::all();
+       return view('index', ['vuelos' => $vuelos]);
+     }
+
+  function show($id = 0) {
+    try {
     $vuelo = Vuelo::findOrFail($id);
     return view('vuelo', ['vuelo' => $vuelo]);
-  }
-  
-  function show($id = 1) {
-    $vuelo = Vuelo::findOrFail($id);
-    return view('vuelo', ['vuelo' => $vuelo]);
+  }catch(ModelNotFoundException $ex){
+    return view('error.exception', ['error.exception' => $ex]);
+          }
 
-
-
-    }
+        }
 }
---
+++ resources/views/error/exception.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>HAS AN ERROR</title>
+  </head>
+  <body>
+    <h1>404 ERROR</h1>
+    <h4>you have made a mistake</h4>
+
+  </body>
+</html>
